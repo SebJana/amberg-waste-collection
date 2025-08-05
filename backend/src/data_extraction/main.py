d2b2bbc3096a67c079a74c8692bc21d4dccca76e
Sebastian Jana <s.jana@oth-aw.de>
2025-08-05 23:08:48 +0200

    fix: adjust file naming

--- backend/src/data_extraction/main.py
+++ backend/src/data_extraction/main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     # Jan - Jun
     run_extraction(
-        pdf_name="Abfuhrplan_Januar_bis_Juni.pdf",
+        pdf_name="Abfuhrplan_Januar_bis_Juni_{YEAR}.pdf",
         box_coords=(135, 320, 3375, 2255),
         csv_name=f"waste-collection-01_06_{YEAR}.csv",
         months=["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     # Jul - Dec
     run_extraction(
-        pdf_name="Abfuhrplan_Juli_bis_Dezember.pdf",
+        pdf_name="Abfuhrplan_Juli_bis_Dezember_{YEAR}.pdf",
         box_coords=(122, 320, 3385, 2258),
         csv_name=f"waste-collection-07_12_{YEAR}.csv",
         months=["Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
